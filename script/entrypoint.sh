03e7ee120442708a2f43bfc5c4bd0c3c2aa87918
Pedro Trujillo <pedro@redmic.es>
2019-02-01 12:35:58 +0000

    Corrige ruta de backup, mejora salida de texto

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,7 +2,8 @@
 
 REDIS_HOST="${REDIS_HOST:-vmq-redis}"
 REDIS_PORT="${REDIS_PORT:-6379}"
-BACKUP_PATH="${BACKUP_PATH:-/redis}"
+BACKUP_PATH="${BACKUP_PATH:-/tmp}"
+PUSHGATEWAY_JOB=${PUSHGATEWAY_JOB:-vmq-redis-backup}
 
 if [ -z "${AWS_BUCKET}" ]
 then
@@ -27,6 +28,8 @@ startSeconds=$(date +%s)
 currentDate=$(date +%Y-%m-%d_%H-%M-%S)
 backupFilename="vmq-redis-dump-${currentDate}.rdb"
 
+echo "Creating backup .."
+
 if redis-cli -h ${REDIS_HOST} -p ${REDIS_PORT} --rdb "${BACKUP_PATH}/${backupFilename}"
 then
 	echo "Backup successfully created"
@@ -43,6 +46,8 @@ then
 	exit 1
 fi
 
+echo "Uploading backup to S3 .."
+
 if aws s3 cp "${BACKUP_PATH}/${backupFilename}" s3://${AWS_BUCKET}
 then
 	echo "Backup successfully uploaded"
@@ -53,15 +58,15 @@ fi
 
 backupDuration=$(( $(date +%s) - startSeconds ))
 
+echo "Publishing metrics .."
+
 if [ -z "${PUSHGATEWAY_HOST}" ]
 then
 	echo "Warning, 'PUSHGATEWAY_HOST' environment variable not defined, metrics cannot be published"
 	exit 0
 fi
 
-PUSHGATEWAY_JOB=${PUSHGATEWAY_JOB:-vmq-redis-backup}
-
-cat <<EOF | curl --data-binary @- ${PUSHGATEWAY_HOST}/metrics/job/${PUSHGATEWAY_JOB}
+cat <<EOF | curl --silent --data-binary @- ${PUSHGATEWAY_HOST}/metrics/job/${PUSHGATEWAY_JOB}
 # HELP backup_db outcome of the backup database job (0=failed, 1=success).
 # TYPE backup_db gauge
 backup_db{label="${REDIS_HOST}"} 1
@@ -75,3 +80,10 @@ backup_size_bytes{label="${REDIS_HOST}"} ${backupSize}
 # TYPE backup_created_date_seconds gauge
 backup_created_date_seconds{label="${REDIS_HOST}"} ${startSeconds}
 EOF
+
+if [ ${?} -eq 0 ]
+then
+	echo "Metrics successfully published"
+else
+	echo "Metrics publish failed!"
+fi
